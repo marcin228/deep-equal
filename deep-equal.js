e86ce7e82b84f40a89b396655d7ef15c3140cb16
Marcin228 <marcin@goingforit.pl>
2024-09-22 14:52:01 +0200

    Add performance tests.

--- deep-equal.js
+++ deep-equal.js
@@ -25,47 +25,63 @@ const deepEqual = function(a, b){
 let a, b;
 const arr = [1,2,3];
 
-// the same shallow objects
-a = { id: 1 };
-b = { id: 1 };
-console.log(true, deepEqual(a, b));
+console.time('total')
 
-// different shallow objects
-a = { id: 1 };
-b = { id: 2 };
-console.log(false, deepEqual(a, b));
+for(let i1 = 0; i1 < 10_000; i1++){
 
-// different types
-a = { id: 1 };
-b = 1;
-console.log(false, deepEqual(a, b));
+    // the same shallow objects
+    a = { id: 1 };
+    b = { id: 1 };
+    if(true != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with the same arrays
-a = { arr: arr, id: 1 };
-b = { arr: arr, id: 1 };
-console.log(true, deepEqual(a, b));
+    // different shallow objects
+    a = { id: 1 };
+    b = { id: 2 };
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with the different arrays
-a = { arr: arr, id: 1 };
-b = { arr: [1,2,3,4], id: 1 };
-console.log(false, deepEqual(a, b));
+    // different types
+    a = { id: 1 };
+    b = 1;
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with the different arrays
-a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
-b = { deep: {arr: [1,2,3,4], num: 0}, id: 1 };
-console.log(false, deepEqual(a, b));
+    // the same objects with the same arrays
+    a = { arr: arr, id: 1 };
+    b = { arr: arr, id: 1 };
+    if(true != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with nested objects with the same arrays
-a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
-b = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
-console.log(true, deepEqual(a, b));
+    // the same objects with the different arrays
+    a = { arr: arr, id: 1 };
+    b = { arr: [1,2,3,4], id: 1 };
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with nested objects with the same arrays and differently named properties
-a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
-b = { deep: {arr: [1,2,3], nums: 0 }, id: 1 };
-console.log(false, deepEqual(a, b));
+    // the same objects with the different arrays
+    a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
+    b = { deep: {arr: [1,2,3,4], num: 0}, id: 1 };
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
 
-// the same objects with nested objects with the same arrays and properties with different values
-a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
-b = { deep: {arr: [1,2,3], num: 1 }, id: 1 };
-console.log(false, deepEqual(a, b));
+    // the same objects with nested objects with the same arrays
+    a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
+    b = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
+    if(true != deepEqual(a, b))
+        throw new Error('test failed.');
+
+    // the same objects with nested objects with the same arrays and differently named properties
+    a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
+    b = { deep: {arr: [1,2,3], nums: 0 }, id: 1 };
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
+
+    // the same objects with nested objects with the same arrays and properties with different values
+    a = { deep: {arr: [1,2,3], num: 0 }, id: 1 };
+    b = { deep: {arr: [1,2,3], num: 1 }, id: 1 };
+    if(false != deepEqual(a, b))
+        throw new Error('test failed.');
+}
+
+console.timeEnd('total');
